28dea5140dcc3dcaffb3f145483eded7c26fab76
ulises-p <ulisesm@cloudmex.io>
2023-01-20 14:17:28 -0800

    added auth

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+import awsExports from './aws-exports';
+import React from 'react';
+Amplify.configure(awsExports);
+
+
+function App({ signOut, user }) {
   return (
-    <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-      </header>
-      <AmplifySignOut />
-    </div>
+    <div>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+      </div>
   );
 }
 
